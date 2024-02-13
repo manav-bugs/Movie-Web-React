d053a55f7d0825f8b3e8c4c53bd83886b7060d13
manav-bugs <manavmodi1102@gmail.com>
2024-02-13 16:51:53 +0530

    render jsx,css

--- src/Movies.js
+++ src/Movies.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { useGlobalContext } from "./context";
+import { NavLink } from "react-router-dom";
 
 const Movies = () => {
   const { movie } = useGlobalContext();
   return (
     <>
-      {movie.map((currMovie) => {
-        return (
-        <div>
-          <h2>{currMovie.Title}</h2>
-        </div>);
-      })}
+      <section className='movie-page'>
+        <div className='container grid grid-4-col'>{movie.map((currMovie) => {
+          const {imdbID,Title,Poster}=currMovie;
+          return(
+            <NavLink to={`movie/${imdbID}`} key={imdbID}>
+               <div className="card">
+                <div className="card-info">
+                  <h2>{Title}</h2>
+                  <img src={Poster} alt={Title} />
+                </div>
+               </div>
+            </NavLink>
+          )
+        })}</div>
+      </section>
     </>
   );
 };
