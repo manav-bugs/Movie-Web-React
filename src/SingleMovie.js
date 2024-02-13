463341136f36dc3db77c587bf7aa342634552d39
manav-bugs <manavmodi1102@gmail.com>
2024-02-13 14:32:15 +0530

    react-router-dom

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const SingleMovie = () => {
+  const {id} =useParams();
   return (
-    <div>SingleMovie</div>
+    <div>Our Single  Movie Page with id: {id}</div>
   )
 }
 
